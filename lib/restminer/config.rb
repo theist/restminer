210eac45b48811535b5c51a3e5011b9fab35d0c7
Carlos Peñas <carlos.penas@the-cocktail.com>
2015-03-31 21:15:26 +0200

    moved connection to config

--- lib/restminer/config.rb
+++ lib/restminer/config.rb
@@ -4,9 +4,16 @@ module Restminer
     attr_accessor :url
     attr_accessor :api_key
 
+    attr_accessor :connection
+
     def initialize(url,api_key)
       self.url=url
       self.api_key=api_key
+
+      self.connection = Faraday.new(url: url) do |f|
+        f.adapter Faraday.default_adapter
+      end
+      @connection.basic_auth(api_key,api_key)
     end
 
     class << self
@@ -14,7 +21,7 @@ module Restminer
         url = ""
         api_key = ""
         if File.exist?(file)
-          File.new("#{ENV['HOME']}/.restminer/config").readlines.each {|line| puts line; eval line}
+          File.new("#{ENV['HOME']}/.restminer/config").readlines.each {|line| eval line}
         end
         Config.new(url,api_key)
       end
--- lib/restminer/redmine.rb
+++ lib/restminer/redmine.rb
@@ -6,18 +6,13 @@ module Restminer
   class Redmine
 
     attr_accessor :config
-    attr_accessor :connection
 
     def initialize
       @config = Config.from_default_file
-      @connection=Faraday.new(url: config.url) do |f|
-        f.adapter Faraday.default_adapter
-      end
-      @connection.basic_auth(config.api_key, config.api_key)
     end
 
     def get_tickets
-      puts @connection.get('/issues.json').body
+      puts @config.connection.get('/issues.json').body
     end
 
     class << self
