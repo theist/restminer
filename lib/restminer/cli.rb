3706058d5d85308431f39b31eb48e85767458a61
Carlos Peñas <carlos.penas@the-cocktail.com>
2015-04-21 23:38:07 +0200

    print ticket primer

--- lib/restminer/cli.rb
+++ lib/restminer/cli.rb
@@ -10,6 +10,19 @@ module Restminer
     def read(id)
       r = Redmine.new
       t = Ticket.new(id)
+      puts "-".green * COLS
+      puts "[#{t.tracker}]".blue + "[##{t.id}]".green + "   #{t.subject}".white
+      puts "-".green * COLS
+      puts "Autor:      ".green + "#{t.author}".red
+      puts "Asignado a: ".green + "#{t.assigned_to}".red
+      puts "Estado:     ".green + "#{t.status}".red
+      puts "Prioridad:  ".green + "#{t.priority}".red
+      #puts "Categoria:  ".green + "#{t.category}".red
+      puts "-".green * COLS
+      puts t.description
+      puts "-".green * COLS
+      puts "#{r.config.url}/issues/#{t.id}"
+      puts "-".green * COLS
     end
   end
 
