83fda40fbf29976e14a30a659352411876339997
Carlos Peñas <carlos.penas@the-cocktail.com>
2015-04-22 00:07:41 +0200

    Redmine user primer

--- lib/restminer.rb
+++ lib/restminer.rb
@@ -1,4 +1,5 @@
 require_relative "restminer/version"
 require_relative "restminer/redmine"
 require_relative "restminer/ticket"
+require_relative "restminer/user"
 require_relative "restminer/cli"
---
+++ lib/restminer/user.rb
@@ -0,0 +1,43 @@
+require 'faraday'
+require 'json'
+require_relative 'config'
+require 'active_model'
+
+module Restminer
+  class User
+  include ActiveModel::Serializers::JSON
+    attr_accessor :config
+
+    @new_user = true
+    def attributes=(hash)
+      hash.each do |k,v|
+        class_eval { attr_accessor k}
+        send("#{k}=", v)
+      end
+    end
+
+    def name
+      return firstname + ' ' + lastname
+    end
+
+    def attributes
+      instance_values
+    end
+
+    def to_s
+      return name
+    end
+
+    def initialize(id = nil)
+      @config = Config.instance
+      raise 'Main Redmine class not configured' unless @config.configured?
+      if id
+        @new_user = false
+        json = @config.connection.get("/users/#{id}.json").body
+        from_json(json,true)
+      end
+    end
+  end
+end
+
+
