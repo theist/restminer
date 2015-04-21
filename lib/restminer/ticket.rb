edbc589c4749bb5ec1c61f7b0d1d9a4f0089f1b0
Carlos Peñas <carlos.penas@the-cocktail.com>
2015-04-21 21:54:02 +0200

    Active model initialization from json

--- lib/restminer/ticket.rb
+++ lib/restminer/ticket.rb
@@ -1,24 +1,43 @@
 require 'faraday'
 require 'json'
 require_relative 'config'
+require 'active_model'
 
 module Restminer
   class Ticket
-
+  include ActiveModel::Serializers::JSON
     attr_accessor :config
+
+    attr_accessor :id, :project, :tracker, :status, :priority,
+                  :author, :assigned_to, :fixed_version, :subject,
+                  :description, :start_date, :done_ratio, :spent_hours,
+                  :created_on, :updated_on, :closed_on
+
     @new_ticket = true
+    def attributes=(hash)
+      hash.each do |k,v|
+        send("#{k}=", v)
+      end
+    end
+
+    def attributes
+      instance_values
+    end
+
+    def to_s
+      return "[Ticket:#{id}] #{subject}"
+    end
 
     def initialize(id = nil)
       @config = Config.instance
       raise 'Main Redmine class not configured' unless @config.configured?
       if id
         @new_ticket = false
-        puts @config.connection.get("/issues/#{id}.json").body
+        json = @config.connection.get("/issues/#{id}.json").body
+        from_json(json,true)
       end
     end
   end
 end
-
-
 
 
--- restminer.gemspec
+++ restminer.gemspec
@@ -29,4 +29,5 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "faraday", "~> 0"
   spec.add_dependency "thor", "~> 0"
+  spec.add_dependency "activemodel"
 end
